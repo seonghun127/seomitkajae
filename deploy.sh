79ab600f7cb74a484cba1d9910bed0e0fc33b1dc
seonghun127 <seonghun127@gmail.com>
2020-07-28 20:42:19 +0900

    docs: deploy.sh 추가

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+REPOSITORY=/opt/application
+cd $REPOSITORY
+
+APP_NAME=서밑까제
+JAR_NAME=$(ls $REPSOITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+        echo "> 종료할 것 없음"
+else
+        echo ">kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
